a8c67668952fd5fc87393cee945e0ba6d3424ec8
maqian48 <maqian6@jd.com>
2017-08-15 11:26:33 +0800

    去除transfer/g/g.go冗余项

--- transporter/g/g.go
+++ transporter/g/g.go
@@ -4,14 +4,6 @@ import (
 	"runtime"
 )
 
-const (
-	GAUGE        = "GAUGE"
-	COUNTER      = "COUNTER"
-	DERIVE       = "DERIVE"
-	DEFAULT_STEP = 60
-	MIN_STEP     = 30
-)
-
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
