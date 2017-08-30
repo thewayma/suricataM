f5f007bbdb32dd48024364bcb0b714ce954a3e63
maqian6 <maqian6@jd.com>
2017-08-30 10:47:09 +0800

    log level调成为warning

--- comm/log/logger.go
+++ comm/log/logger.go
@@ -9,9 +9,9 @@ var Log = make(log4go.Logger)
 //!< 日志等级从低到高: FINEST, FINE, DEBUG, TRACE, INFO, WARNING, ERROR, CRITICAL
 func init() {
 	file := log4go.NewFileLogWriter("run.log", true)
-	file.SetRotateLines(3600)
+	file.SetRotateLines(10000)
 	file.SetRotateDaily(true)
-	Log.AddFilter("file", log4go.DEBUG, file)
+	Log.AddFilter("file", log4go.WARNING, file)
 
 	Log.Info("Log Framework Start")
 }
